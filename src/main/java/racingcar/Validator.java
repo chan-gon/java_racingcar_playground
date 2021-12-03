a8274d43b1ddd0a29ba9ccf2bfe36694f58ab88e
Charlie <ruggedcafe@gmail.com>
2021-12-03 23:11:35 +0900

    refactor: ValidatorTest 테스트 로직 수정
    자동차 이름 길이 초과 예외 처리 기능 추가

--- src/main/java/racingcar/Validator.java
+++ src/main/java/racingcar/Validator.java
@@ -3,6 +3,9 @@ package racingcar;
 public class Validator {
 
     public boolean carNameLengthCheck(String carName) {
+        if (carName.length() > 6) {
+            throw new IllegalArgumentException();
+        }
         return carName.length() < 6;
     }
 }
--- src/test/java/racingcar/ValidatorTest.java
+++ src/test/java/racingcar/ValidatorTest.java
@@ -1,10 +1,12 @@
 package racingcar;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class ValidatorTest {
 
@@ -16,9 +18,16 @@ class ValidatorTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"java, true", "mysql, true", "lamborghini, false"})
+    @CsvSource({"java, true", "mysql, true"})
     void 자동차_이름_길이_검증(String carName, boolean result) {
         assertThat(validator.carNameLengthCheck(carName)).isEqualTo(result);
     }
 
+    @Test
+    void 자동차_이름_길이_초과_예외_출력_검증() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+           String input = "lamborghini";
+           validator.carNameLengthCheck(input);
+        });
+    }
 }
